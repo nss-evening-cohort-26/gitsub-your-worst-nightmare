8a95e83450d3f483320ea79f79dd1b53a9343b32
taylorseager <taylorseager718@gmail.com>
2024-02-11 21:36:54 -0600

    almost finished Projects page

--- main.js
+++ main.js
@@ -8,25 +8,25 @@ const projects = [
     id: 2,
     projectName: "Weather App",
     projectDescription:
-      "A Python project utilizing API calls to provide real-time weather information, featuring a user-friendly command-line interface for easy access to current weather conditions and forecasts.",
+      "A Python project utilizing API calls to provide real-time weather information.",
   },
   {
     id: 3,
     projectName: "To Do List",
     projectDescription:
-      "A web-based task management application built with React and Node.js, allowing users to create, update, and delete tasks while providing a clean and responsive user interface.",
+      "A web-based task management application built with React and Node.js.",
   },
   {
     id: 4,
     projectName: "Image Classifier",
     projectDescription:
-      "A machine learning project using TensorFlow and Python to develop an image classification model, capable of accurately identifying objects within images based on a pre-trained neural network.",
+      "A project using TensorFlow and Python to develop an image classification model.",
   },
   {
     id: 5,
     projectName: "Expense Tracker",
     projectDescription:
-      "A mobile app developed with Flutter and Firebase, designed to help users track their expenses, set budgets, and visualize spending patterns through interactive charts and graphs.",
+      "A mobile app developed with Flutter and Firebase.",
   },
 ];
 
@@ -34,11 +34,8 @@ const renderToDom = (divId, htmlToRender) => {
   const selectedDiv = document.querySelector(divId);
   selectedDiv.innerHTML = htmlToRender;
 };
-// TODO: ts queryselector projects-n
-// TODO: ts eventlistener that listens for projects-n
-// TODO: ts renderToDom to inject projectPage
-// for Projects Page //
 
+// for Projects Page //
 const switchProjectPage = document.querySelector("#projects-n");
 
 // this is for the project list
@@ -46,7 +43,7 @@ const projectsListOnDom = (projects) => {
   let domString = "";
   projects.map((project) => {
     domString += `<div class="container text-center">
-    <div class="row row-cols-xs-1 row-cols-md-2 row-cols-md-3">
+    <div class="row row-cols-sm-3 row-cols-sm-3 row-cols-sm-3">
       <div class="col">${project.projectName}</div>
       <div class="col">${project.projectDescription}</div>
       <div class="col">...</div>
@@ -57,35 +54,27 @@ const projectsListOnDom = (projects) => {
 };
 projectsListOnDom(projects);
 
-// // project form
-const projectFormTemplate = () => {
-  console.log("kittens")
-  let domString = 
-  `<div class="mb-3">
-    <label for="projectNameForm" class="form-label">Project Name</label>
-    <input type="email" class="form-control" id="projectNameForm">
-  </div>
-  <div class="mb-3">
-    <label for="formDescription" class="form-label">Enter project description here</label>
-    <textarea class="form-control" id="formDescription" rows="3"></textarea>
-  </div>`
-  renderToDom("#projectForm", domString);
+
+const newProjectForm = document.querySelector("#projectFormSubmit");
+
+const createProject = (e) => {
+  e.preventDefault();
+
+  const newProject = {
+    id: projects.length + 1,
+    projectName: document.querySelector("#projectNameForm").value,
+    projectDescription: document.querySelector("#formDescription").value,
+  };
+
+  projects.push(newProject);
+  projectsListOnDom(projects);
+  newProjectForm.reset();
 };
-projectFormTemplate()
 
-// const projectForm = (e) => {
-//   e.preventDefault();
+newProjectForm.addEventListener("submit", createProject);
 
-//   const projectForm = {
-//     id: projects.length + 1,
-//     projectName: document.querySelector("projectNameForm").value,
-//     projectDescription: document.querySelector("formDescription").value,
-//   };
 
-//   projects.push(projectForm);
-//   projectsListOnDom(projects);
-//   form.reset();
-// };
+
 // switchProjectPage.addEventListener("click", (e) => {
 //   console.log("clicked")
 //   if (e.target.id === "projects-n") {
